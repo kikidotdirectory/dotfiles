def12112664c5945c2befd1faa9cc9292eab912b
kiki chang <inbox@kiki.directory>
2026-07-18 17:50:41 -0400

    new: set up install script

---
+++ install/install-config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ln -sf "$DOTFILES/config/leaderkey" "$HOME/.config"
+ln -sf "$DOTFILES/config/yashiki" "$HOME/.config"
+ln -sf "$DOTFILES/config/direnv" "$HOME/.config"
+ln -sf "$DOTFILES/config/aerospace" "$HOME/.config"

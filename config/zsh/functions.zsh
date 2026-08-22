36a3f2f3b3f2b4eff65c34d50f17a4224f9e19d8
kiki chang <inbox@kiki.directory>
2026-08-22 18:42:06 -0400

    update helper wrapper functions

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -5,23 +5,60 @@
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
 	yazi "$@" --cwd-file="$tmp"
-	IFS= read -r -d '' cwd < "$tmp"
+	IFS= read -r -d '' cwd <"$tmp"
 	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
 	rm -f -- "$tmp"
 }
 
-# Helper to quickly navigate to ~/Projects
-p() {
-  if [[ -z "$1" ]]; then
-    cd ~/Projects
-  else
-    cd ~/Projects/"$1"
-  fi
+tc() {
+	if [ "$1" = "cd" ]; then
+		cd "$(command tc cd)"
+	else
+		command tc "$@"
+	fi
 }
 
-_p() {
-  local -a dirs
-  dirs=(~/Projects/*(/N:t))
-  _describe 'project' dirs
+di() {
+	dprint init "$@"
+	jq '.useTabs = true' dprint.json >tmp.json && mv tmp.json dprint.json
 }
-compdef _p p
+
+# Dir jump helpers
+function cd_dir() {
+	local base="$1"
+	local query="$2"
+	local target
+	target=$(fd --type d --base-directory "$base" -i "$query" |
+		awk '{ print gsub(/\//,"/"), $0 }' | sort -n | head -1 | cut -d' ' -f2-)
+	if [ -n "$target" ]; then
+		cd "$base/$target"
+	else
+		echo "No match for '$query'"
+	fi
+}
+
+make_cd_shortcut() {
+	local name="$1"
+	local base="$2"
+
+	# Define the wrapper function, e.g. `c`, `u`, `p`, etc.
+	eval "
+  ${name}() {
+    cd_dir \"$base\" \"\$1\"
+  }
+  "
+	# Define its matching completion function
+	eval "
+  _${name}_complete() {
+    local -a dirs
+    dirs=(\${(f)\"\$(fd --type d --base-directory \"$base\" . 2>/dev/null | xargs -n1 basename)\"})
+    _describe 'directory' dirs
+  }
+  "
+
+	compdef "_${name}_complete" "$name"
+}
+
+# directories
+make_cd_shortcut c "$HOME/dotfiles"
+make_cd_shortcut p "$HOME/Projects"

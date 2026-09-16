c5a5736b20205c47b825389f2549d1a5e91ea532
calcifer chang <ckc@desktop.local>
2026-09-16 00:25:30 -0400

    update dependencies
    ensure 1pass sock is enabled
    ensure SbarLua is installed for sketchybar

--- install/install-utilities.sh
+++ install/install-utilities.sh
@@ -5,7 +5,20 @@ set -o pipefail
 mkdir -p "$DOTFILES/utilities"
 
 if [[ ! -d "$DOTFILES/utilities/tc" ]]; then
+  op_ssh_socket="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+  if [[ ! -S "$op_ssh_socket" ]]; then
+    echo "error: 1Password SSH agent socket not found at $op_ssh_socket" >&2
+    echo "  open 1Password, sign in, and enable Settings > Developer > Use the SSH Agent, then re-run" >&2
+    exit 1
+  fi
+
   git clone git@github.com:kikidotdirectory/tc.git "$DOTFILES/utilities/tc"
 fi
 
 (cd "$DOTFILES/utilities/tc" && npm install && npm run build && npm link)
+
+if [[ ! -d "$DOTFILES/utilities/SbarLua" ]]; then
+  git clone --depth 1 https://github.com/FelixKratz/SbarLua.git "$DOTFILES/utilities/SbarLua"
+fi
+
+(cd "$DOTFILES/utilities/SbarLua" && make install)
